d530469ba80842993933f7e653dbcaf4991aa2b1
Matsuel <matsuel0308@gmail.com>
2024-10-02 22:19:19 +0200

    Page Home -> Add img and paragrpah

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 import Gymnase from "@/assets/Gymnase.png";
 import Logo from "@/assets/cacbo.svg";
 import GPS from "@/assets/gps.svg";
+import Button from "@/components/button";
+import GoTo from "@/assets/goto.svg";
+import Groupe from "@/assets/photo groupe.png";
 
 
 export default function Home() {
@@ -40,7 +43,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="w-[48%] h-auto mt-28 mb-36 flex flex-col relative">
+      <div className="w-[39%] h-auto mt-28 mb-36 flex flex-col relative">
         <div className="font-SourceSerifPro text-9xl text-white text-center absolute top-[-70px] left-[-30px] z-0">
           «
         </div>
@@ -48,9 +51,47 @@ export default function Home() {
           Un club où la <strong className="font-medium">passion</strong> du badminton se vit dans une ambiance <strong className="font-medium">familiale</strong> et conviviale.
           Que vous soyez débutant ou expert, jouez et progressez dans un cadre <strong className="font-medium">chaleureux</strong>, sans prise de tête.
         </p>
-        <div className="font-SourceSerifPro text-9xl text-white text-center absolute bottom-[-40px] right-[390px] z-0">
+        <div className="font-SourceSerifPro text-9xl text-white text-center absolute bottom-[-40px] right-[140px] z-0">
           »
         </div>
+      </div>
+
+      <div className="w-[80%] h-auto flex flex-col items-start justify-center gap-5">
+        <h2 className="font-Montserrat font-semibold text-3xl">Qui sommes-nous ?</h2>
+
+        <div className="w-[100%] h-auto flex flex-row items-start justify-center gap-32">
+
+          {/* Texte gauche */}
+          <div className="w-[40%] h-auto flex flex-col items-center justify-center gap-3 bg-white rounded-ten p-5">
+
+            <p className="font-Montserrat font-light text-xl text-start">
+              Créé en 2007 à Carbon-Blanc, notre club réunit des passionnés de badminton de tous niveaux.
+              Ici, l’ambiance est familiale et conviviale, <strong className="font-medium">propice à la détente</strong> et au partage.
+              Que vous soyez compétiteur ou simplement en quête de loisirs, vous trouverez chez nous un cadre chaleureux pour <strong className="font-medium">pratiquer sans pression</strong>. Envie d'en savoir plus ?
+            </p>
+
+            <div className="w-[100%] h-auto flex flex-row items-center justify-end">
+              <Button variant="inverted" href="/about">
+                DECOUVRIR NOTRE HISTOIRE
+                <Image src={GoTo} alt="goto" className="ml-2" />
+              </Button>
+            </div>
+
+          </div>
+
+          {/* Image droite */}
+          <div className="w-[50%] h-auto flex flex-col items-center justify-center gap-3 bg-white rounded-ten">
+
+            <Image src={Groupe} alt="groupe" />
+
+            <p className="w-[60%] font-Montserrat font-light text-base text-center">
+              Nous organisons régulièrement des soirées, améliorant la cohésion entre nos membres (mars 2024)
+            </p>
+
+          </div>
+
+        </div>
+
       </div>
 
     </div>
