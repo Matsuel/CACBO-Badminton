5b4a4ead80efeed725415913d35502629ad663dd
Matsuel <matsuel0308@gmail.com>
2024-10-14 19:31:32 +0200

    Delete unuseds imports

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { StaticImport } from 'next/dist/shared/lib/get-img-props'
-import Image, { StaticImageData } from 'next/image'
+import Image from 'next/image'
 import React from 'react'
 import Open from './Open'
 import HeroBackground from '@/assets/hero.png'
