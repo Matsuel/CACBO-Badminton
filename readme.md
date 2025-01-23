c1d3faf00e25a48ef195102d1edf299605bff83b
Matsuel <matsuel0308@gmail.com>
2025-01-23 19:31:47 +0100

    Pages -> Mentions legales

--- readme.md
+++ readme.md
@@ -1,12 +1,2 @@
-# Composants à faire afin de créer la responsive plus facilement
-
-
-- Composants à faire:
-    - ListElement (className, children, afficher ou non le point de début)
-        - Adhérer, Contact,Règles
-    - ListTitle (classNme, children)
-        - Adhérer, Contact, Règles
-    - CACBOLink (className, children, href, target)
-        - Adhérer, Contact
-    - DivBlanche (className, children)
-        - Home, Histoire, Adhérer, Contact, Règles
+Revoir composants
+Passer sur le app router
--- src/pages/mentions-legales.tsx
+++ src/pages/mentions-legales.tsx
@@ -25,7 +25,7 @@ const MentionsLegales = () => {
 
                         <Paragraphe><strong className="font-medium">Directeur de la publication :</strong> Maël THOMAS.</Paragraphe>
 
-                        <Paragraphe><strong className="font-medium">Contact</strong> : <Link href={"mailto:cacbo@gmail.com"} className="underline">cacbo.bad@hotmail.com</Link></Paragraphe>
+                        <Paragraphe><strong className="font-medium">Contact</strong> : <Link href={"mailto:cacbo.bad@hotmail.com"} className="underline">cacbo.bad@hotmail.com</Link></Paragraphe>
                     </div>
                 </div>
 
@@ -46,6 +46,54 @@ const MentionsLegales = () => {
                     </Subtitle>
                     <div className="w-full h-auto flex flex-col items-start justify-start gap-1 bg-white p-[30px] rounded-ten">
                         <Paragraphe>Tous les contenus présents sur ce site (textes, images, logos, etc.) sont la propriété exclusive du CACBO Badminton ou sont utilisés avec autorisation. Toute reproduction, distribution, modification ou utilisation de ces contenus sans autorisation préalable est interdite.</Paragraphe>
+                    </div>
+                </div>
+
+                <div className="w-[100%] h-auto flex flex-col items-start justify-start mt-[20px] gap-[30px]">
+                    <Subtitle>
+                        Données personnelles
+                    </Subtitle>
+                    <div className="w-full h-auto flex flex-col items-start justify-start gap-2 bg-white p-[30px] rounded-ten">
+                        <Paragraphe>Dans le cadre des adhésions, le CACBO Badminton collecte et traite des données personnelles via la plateforme HelloAsso. Ces données sont nécessaires pour gérer les inscriptions, établir les factures et communiquer avec les membres.</Paragraphe>
+
+                        <Paragraphe>Les données sont gérées conformément au Règlement Général sur la Protection des Données (RGPD). Vous pouvez consulter la politique de confidentialité d’HelloAsso sur leur site (helloasso.com).</Paragraphe>
+
+                        <Paragraphe>Aucune donnée personnelle n’est directement collectée via ce site internet.</Paragraphe>
+
+                        <Paragraphe>Pour toute question relative à vos données personnelles, ou pour exercer vos droits d’accès, de rectification ou de suppression, vous pouvez contacter le club à l’adresse suivante :</Paragraphe>
+
+                        <Paragraphe>
+                            <strong className='font-medium'>CACBO Badminton</strong> <br /> {/*Dégager cette daube au plus vite */}
+                            <strong className='font-medium'>24 rue Pasteur, 33560 Carbon-Blanc</strong> <br />
+                            ou par e-mail à : <Link href={"mailto:cacbo.bad@hotmail.com"} className='underline'>cacbo.bad@hotmail.com</Link>
+                        </Paragraphe>
+                    </div>
+                </div>
+
+                <div className="w-[100%] h-auto flex flex-col items-start justify-start mt-[20px] gap-[30px]">
+                    <Subtitle>
+                        Cookies
+                    </Subtitle>
+                    <div className="w-full h-auto flex flex-col items-start justify-start gap-1 bg-white p-[30px] rounded-ten">
+                        <Paragraphe>Ce site peut utiliser des cookies pour améliorer votre expérience utilisateur (par exemple, pour enregistrer des préférences ou analyser les visites). Aucun cookie publicitaire ou de suivi tiers n'est utilisé sans votre consentement explicite.</Paragraphe>
+                    </div>
+                </div>
+
+                <div className="w-[100%] h-auto flex flex-col items-start justify-start mt-[20px] gap-[30px]">
+                    <Subtitle>
+                        Responsabilité
+                    </Subtitle>
+                    <div className="w-full h-auto flex flex-col items-start justify-start gap-1 bg-white p-[30px] rounded-ten">
+                        <Paragraphe>Le CACBO Badminton met tout en œuvre pour assurer l'exactitude des informations diffusées sur le site. Toutefois, il ne peut être tenu responsable d’éventuelles erreurs ou omissions, ni des dommages liés à l’utilisation de ces informations ou de ce site.</Paragraphe>
+                    </div>
+                </div>
+
+                <div className="w-[100%] h-auto flex flex-col items-start justify-start mt-[20px] gap-[30px]">
+                    <Subtitle>
+                        Modification des mentions légales
+                    </Subtitle>
+                    <div className="w-full h-auto flex flex-col items-start justify-start gap-1 bg-white p-[30px] rounded-ten">
+                        <Paragraphe>Le CACBO Badminton se réserve le droit de modifier les présentes mentions légales à tout moment.</Paragraphe>
                     </div>
                 </div>
 
