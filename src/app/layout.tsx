c60f09bcef2d4ee0f7b02ccdef91d04d2832c93e
Matsuel <matsuel0308@gmail.com>
2025-01-29 16:30:36 +0100

    Ajout métadatas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,27 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { SITE_CONFIG } from "@/constantes";
+import { BASE_URL } from "./sitemap";
 
 export const metadata: Metadata = {
   title: SITE_CONFIG.title,
   description: "Cacbo Badminton Club",
+  keywords: ["badminton", "cacbo", "club", "badminton club", "badminton club cacbo"],
+  openGraph: {
+    type: "website",
+    locale: "fr_FR",
+    url: BASE_URL,
+    title: SITE_CONFIG.title,
+    description: "Cacbo Badminton Club",
+    images: [
+      {
+        url: `${BASE_URL}/favicon.ico`,
+        width: 800,
+        height: 600,
+        alt: "Cacbo Badminton Club",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
--- src/app/robot.ts
+++ src/app/robot.ts
@@ -16,5 +16,6 @@ export default function robots(): MetadataRoute.Robots {
             },
         ],
         sitemap: `${BASE_URL}/sitemap.xml`,
+        host: BASE_URL,
     }
 }
