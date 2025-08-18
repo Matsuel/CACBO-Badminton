f66aaeef6f367545731df8c5b7e3d24785195b3b
Matsuel <matheolang@icloud.com>
2025-08-18 22:15:56 +0200

    feat: ajout de la constante textVariants pour les styles typographiques

---
+++ src/constantes/text.ts
@@ -0,0 +1,9 @@
+export const textVariants = {
+    h1: "font-Space-Grotesk text-[56px] leading-[38px] font-bold",
+    h2: "font-Space-Grotesk text-[25px] leading-[20px] font-bold",
+    h3: "font-Space-Grotesk text-[21px] leading-[20px] font-bold",
+    h4: "font-Poppins text-[17px] leading-[16px] font-semibold",
+    text: "font-Poppins text-[17px] leading-[16px] font-normal",
+    secondary: "font-Poppins text-[14px] leading-[16px] font-normal",
+    cta: "font-Poppins text-[13px] leading-[16px] font-normal"
+}
