c85689f8668e6495883a99482ea51b9d33c85834
Matsuel <matheolang@icloud.com>
2025-08-20 23:26:03 +0200

    feat: ajout de la police 'Darker Grotesque' et mise à jour de la variante h1

--- src/constantes/theme.ts
+++ src/constantes/theme.ts
@@ -1,5 +1,5 @@
 export const textVariants = {
-    h1: "font-Space-Grotesk text-[56px] leading-[38px] font-bold",
+    h1: "font-Darker-Grotesque text-[56px] leading-[38px] font-bold",
     h2: "font-Space-Grotesk text-[25px] leading-[20px] font-bold",
     h3: "font-Space-Grotesk text-[21px] leading-[20px] font-bold",
     h4: "font-Poppins text-[17px] leading-[16px] font-semibold",
