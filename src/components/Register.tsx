6ee2c912f8fb23d5b4d6ab701b57899271b4729e
Matsuel <matheolang@icloud.com>
2025-08-20 22:27:38 +0200

    Ajout composant affichage état inscriptions

---
+++ src/components/Register.tsx
@@ -0,0 +1,32 @@
+import { textVariants } from '@/constantes/theme';
+import { Check, X } from 'lucide-react';
+import React from 'react'
+
+interface RegisterProps {
+    saison: string;
+    open: boolean;
+}
+
+const Register = ({
+    saison,
+    open
+}: RegisterProps) => {
+    return (
+        <div className='w-full h-auto flex flex-row items-center justify-start p-2 border border-redcolor rounded-full'>
+            <span className={`${textVariants.secondary} text-redpale`}>
+                SAISON 20{saison} -
+            </span>
+
+            <span className={`${textVariants.secondary} text-redcolor font-semibold`}>
+                INSCRIPTIONS {open ? "OUVERTES" : "FERMÉES"} !
+            </span>
+
+            {open ? 
+            <Check strokeWidth={3} className='text-redcolor' /> : <X strokeWidth={3} />
+            }
+            
+        </div>
+    )
+}
+
+export default Register
