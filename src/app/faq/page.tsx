739a6142f99c53220d50010f69589f3f5e1dd80d
Matsuel <matheolang@icloud.com>
2025-08-25 22:49:38 +0200

    Debut page FAQ

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,8 +1,30 @@
+import Content from "@/components/Content"
+import Header from "@/components/Header"
 import PageLayout from "@/components/PageLayout"
+import Image from "next/image"
 
 const Faq = () => {
     return (
         <PageLayout>
+
+            <Header
+                title="FAQ"
+                subtitle="Toutes les infos pratiques sur notre club !"
+            />
+
+            <Content>
+                <Image
+                    src="/faq/1.png"
+                    alt="Photo d'un match"
+                    className="rounded-xl"
+                    width={2000}
+                    height={2000}
+                />
+
+                <div className="flex flex-col gap-5">
+                    
+                </div>
+            </Content>
 
         </PageLayout>
     )
