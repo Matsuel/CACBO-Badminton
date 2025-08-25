e0525bab42db568770cc96a2f049b52b19b377d9
Matsuel <matheolang@icloud.com>
2025-08-25 14:26:30 +0200

    feat: ajout des liens et icônes des réseaux sociaux dans le composant contact

---
+++ src/constantes/contact.tsx
@@ -0,0 +1,22 @@
+import Facebook from '@/assets/footer/Facebook'
+import Instagram from '@/assets/footer/Instagram'
+import Messenger from '@/assets/footer/Messenger'
+
+
+export const SocialLinks = [
+    {
+        icon: <Facebook size={30} />,
+        link: 'https://www.facebook.com/cacbo.bad',
+        name: 'Facebook'
+    },
+    {
+        icon: <Instagram size={30} />,
+        link: 'https://www.instagram.com/cacbo.bad',
+        name: 'Instagram'
+    },
+    {
+        icon: <Messenger size={30} />,
+        link: 'https://www.messenger.com/cacbo.bad',
+        name: 'Messenger'
+    },
+]
