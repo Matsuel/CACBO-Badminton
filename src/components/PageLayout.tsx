d4896cdd8a07ed1ab27c9d4896138d3d3bcb3bca
Matsuel <matheolang@icloud.com>
2025-08-25 13:50:57 +0200

    Ajustement gap

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -14,7 +14,7 @@ const PageLayout = ({
     ...props
 }: PageLayoutProps) => {
     return (
-        <div className={cn('w-full h-auto min-h-screen flex flex-col items-center justify-start bg-background', className)} {...props}>
+        <div className={cn('w-full h-auto min-h-screen flex flex-col items-center justify-start bg-background gap-header', className)} {...props}>
             <div className='w-full flex flex-col items-center justify-start gap-header p-5'>
                 <Navbar />
                 {children}
