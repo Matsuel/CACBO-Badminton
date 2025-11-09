06103ade4f345936625a3298f550d8f8e4fabdf4
Matsuel <matheolang@icloud.com>
2025-11-09 15:45:00 +0100

    Refactor le composant Register pour simplifier la structure et améliorer la lisibilité

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,6 +1,4 @@
 import { textVariants } from '@/constantes/theme';
-import { Check, X } from 'lucide-react';
-import React from 'react'
 
 interface RegisterProps {
     saison: string;
@@ -12,20 +10,9 @@ const Register = ({
     open
 }: RegisterProps) => {
     return (
-        <div className='w-full h-auto flex flex-row items-center justify-start p-2 border border-redcolor rounded-full'>
-            <span className={`${textVariants.secondary} text-redpale`}>
-                SAISON 20{saison} -
-            </span>
-
-            <span className={`${textVariants.secondary} text-redcolor font-semibold`}>
-                INSCRIPTIONS {open ? "OUVERTES" : "FERMÉES"} !
-            </span>
-
-            {open ?
-                <Check strokeWidth={3} className='text-redcolor ml-2' /> : <X strokeWidth={3} className='ml-2' />
-            }
-
-        </div>
+        <p className={`w-full h-auto p-2 border border-redcolor rounded-full ${textVariants.secondary} text-redpale text-center`}>
+            SAISON 20{saison} - <span className="font-semibold">INSCRIPTIONS {open ? "OUVERTES" : "FERMÉES"} ! ✔️</span>
+        </p>
     )
 }
 
