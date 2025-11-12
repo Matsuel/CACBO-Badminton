cfb62c3ef0b115d044a8ff186c82c47aa2bc4d94
Matsuel <matheolang@icloud.com>
2025-11-12 21:16:19 +0100

    Ajout de la dépendance @vercel/speed-insights et intégration du composant SpeedInsights dans le layout principal.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import "./globals.css";
 import { BASE_URL } from "./sitemap";
@@ -36,6 +37,7 @@ export default function RootLayout({
       <body
         className="antialiased"
       >
+        <SpeedInsights />
         {children}
       </body>
     </html>
