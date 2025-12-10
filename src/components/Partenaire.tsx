ba033a3ac3eb4b6b2f5ad908a6458b82a187e4eb
Matsuel <matheolang@icloud.com>
2025-12-10 21:15:23 +0100

    Ajout de Google Analytics et mise à jour des dépendances : intégration de nextjs-google-analytics et zod, et amélioration de la structure des composants.

--- src/components/Partenaire.tsx
+++ src/components/Partenaire.tsx
@@ -1,45 +1,38 @@
-import { textVariants } from '@/constantes/theme';
-import Image from 'next/image';
+import Image from "next/image";
+import { textVariants } from "@/constantes/theme";
 
 interface PartenaireProps {
-    name: string;
-    role: string;
-    image: string;
+	name: string;
+	role: string;
+	image: string;
 }
 
-const Partenaire = ({
-    image,
-    name,
-    role
-}: PartenaireProps) => {
-    return (
-        <div className={"w-full h-auto flex flex-row items-center justify-start p-5 gap-[25px] bg-white rounded-xl shadow-default"}>
+const Partenaire = ({ image, name, role }: PartenaireProps) => {
+	return (
+		<div
+			className={
+				"w-full h-auto flex flex-row items-center justify-start p-5 gap-[25px] bg-white rounded-xl shadow-default"
+			}
+		>
+			<div className="w-[100px] h-[100px] flex items-center justify-center rounded-xl bg-whitebroken shrink-0">
+				<Image
+					src={image}
+					alt="volant de badminton"
+					width={100}
+					height={100}
+					className="rounded-xl w-auto h-auto"
+				/>
+			</div>
 
-            <div
-                    className='w-[100px] h-[100px] flex items-center justify-center rounded-xl bg-whitebroken shrink-0'
-            >
-                    <Image
-                        src={image}
-                        alt='volant de badminton'
-                        width={100}
-                        height={100}
-                        className='rounded-xl w-auto h-auto'
-                    />
-            </div>
+			<div className="flex flex-col gap-content">
+				<div className="flex flex-col">
+					<span className={`${textVariants.h3} text-redcolor`}>{name}</span>
+				</div>
 
-            <div className='flex flex-col gap-content'>
-                <div className='flex flex-col'>
-                    <span className={`${textVariants.h3} text-redcolor`}>{name}</span>
-                </div>
+				<p className={textVariants.text}>{role}</p>
+			</div>
+		</div>
+	);
+};
 
-                <p className={textVariants.text}>
-                    {role}
-                </p>
-            </div>
-
-
-        </div>
-    )
-}
-
-export default Partenaire
+export default Partenaire;
