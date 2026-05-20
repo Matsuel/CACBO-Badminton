85d3e9515290cea3e63c573e2d8f3b9a10065b4d
Matsuel <matheolang@icloud.com>
2026-05-20 13:57:32 +0200

    style: remove commented-out gradient class from President component

--- src/components/President.tsx
+++ src/components/President.tsx
@@ -14,8 +14,6 @@ const President = ({
     ...props
 }: PresidentProps) => {
 
-    // bg gradient-to-r from-redpale to-redcolor
-
     return (
         <div className={cn("w-full flex flex-row items-center px-5 py-3.5 bg-linear-to-tr from-redpale to-redcolor rounded-xl gap-3", className)} {...props}>
             <h3 className={`w-[30%] ${textVariants.h4} text-white`}>
