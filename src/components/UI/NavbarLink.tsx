f0439cd8a4bbbfcaa9e42182d7872f77fc91e0ef
Matsuel <matheolang@icloud.com>
2026-09-09 20:30:13 +0200

    fix: update Navbar and NavbarLink components for consistent background color and improved layout

--- src/components/UI/NavbarLink.tsx
+++ src/components/UI/NavbarLink.tsx
@@ -1,31 +1,26 @@
-import { textVariants } from '@/constantes/theme'
-import type { NavbarLinkType } from '@/index'
-import { ChevronRight } from 'lucide-react'
-import Link from 'next/link'
+import { ChevronRight } from "lucide-react";
+import Link from "next/link";
+import { textVariants } from "@/constantes/theme";
+import type { NavbarLinkType } from "@/index";
 
-const NavbarLink = ({
-    title,
-    href,
-    description,
-    icon
-}: NavbarLinkType) => {
-    return (
-        <Link
-            href={href}
-            className='w-full h-auto p-5 text-redcolor flex flex-row justify-start gap-6 items-center'
-        >
-            <div className='w-12 h-12 flex items-center justify-center bg-redcolor text-white rounded-xl'>
-                {icon}
-            </div>
-            <div className='w-[70%] h-auto flex flex-col gap-3 items-start'>
-                <h3 className={textVariants.h3}>{title}</h3>
-                {description && <p className={`${textVariants.secondary} w-3/4`}>
-                    {description}
-                </p>}
-            </div>
-            <ChevronRight className='w-4' />
-        </Link>
-    )
-}
+const NavbarLink = ({ title, href, description, icon }: NavbarLinkType) => {
+	return (
+		<Link
+			href={href}
+			className="w-full h-auto p-5 text-redcolor flex flex-row justify-start gap-6 items-center bg-white"
+		>
+			<div className="w-12 h-12 flex items-center justify-center bg-redcolor text-white rounded-xl">
+				{icon}
+			</div>
+			<div className="w-[65%] h-auto flex flex-col gap-3 items-start">
+				<h3 className={textVariants.h3}>{title}</h3>
+				{description && (
+					<p className={`${textVariants.secondary} w-3/4`}>{description}</p>
+				)}
+			</div>
+			<ChevronRight className="w-4" />
+		</Link>
+	);
+};
 
-export default NavbarLink
+export default NavbarLink;
